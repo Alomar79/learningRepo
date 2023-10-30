b006102517883473faf756411e2c2bcee7032eb3
Aowad Alomar <aowad.s.omar@gmail.com>
2023-10-30 11:44:08 +0300

    initial commit--1

---
+++ BoxStatus.java
@@ -0,0 +1,16 @@
+package org.example;
+
+/*
+ * BoxStatus.java
+ *
+ * This is an enumerated type which represents the status of a box in a noughts-and-crosses board: Empty, Human, or Computer
+ */
+
+
+/**
+ *
+ * @author ereiter
+ */
+public enum BoxStatus {Empty, Human, Computer
+
+}
