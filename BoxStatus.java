4151dbd278e0ee542ed51b78a1f15d35db4073ee
Alomar79 <aowad.s.omar@gmail.com>
2023-10-30 21:08:57 +0300

    initial commit--4

--- BoxStatus.java
+++ BoxStatus.java
@@ -5,7 +5,7 @@ package org.example;
  *
  * This is an enumerated type which represents the status of a box in a noughts-and-crosses board: Empty, Human, or Computer
  */
-
+7777777777777777777
 
 /**
  *
